bbc9951a06e0712e644637fa1a5be9de16fcfb4f
Juan Camilo López Morales <jclopezm@garantiascomunitarias.com>
2023-04-17 09:26:46 -0500

    Cambios en la presentación de la prueba para AngularJS

--- web/js/app/peliculas.js
+++ web/js/app/peliculas.js
@@ -5,12 +5,12 @@ app.controller("PeliculasController", [
     $scope.listMovies = [];
     $scope.setActive("menuPeliculas");
 
-    $http
-      .get(
-        "https://api.themoviedb.org/3/movie/550?api_key=YOU_KEY_ACCESS"
-      )
-      .then(function (res) {
-        console.log(res.data);
-      });
+    // $http
+    //   .get(
+    //     "https://api.themoviedb.org/3/movie/550?api_key=YOU_KEY_ACCESS"
+    //   )
+    //   .then(function (res) {
+    //     console.log(res.data);
+    //   });
   },
 ]);
